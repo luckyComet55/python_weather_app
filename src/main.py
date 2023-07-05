620df39bcfbb9a1f594fc856a5041ba54b9cb423
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-07-05 17:21:15 +0300

    Added second route for hour precised forecast

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from . import weather_module
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {
-        'message': 'To use out app, provide the name os the city in query'
-    }
+    return 'OK'
+
+@app.get('/weather_current')
+async def get_weather(response: Response, city: str):
+    res = weather_module.get_city_weather(city)
+    response.status_code = res['response_code']
+    return res['data']
+
+@app.get('/weather_forecast')
+async def get_weather_at(response: Response, city: str, time_to_hours: str):
+    hours_accurate = time_to_hours // 3
+    res = weather_module.get_weather_hours(city, hours_accurate)
+    response.status_code = res['response_code']
+    return res['data']
--- src/weather_module.py
+++ src/weather_module.py
@@ -1,16 +1,53 @@
 import requests
 
+__api_key__: str = '9964deeb468addac49d151062b0929e2'
 
 def get_city_weather(city: str):
-    city = 'Moscow'
-
-    url = 'https://api.openweathermap.org/data/2.5/weather?\
-    q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
-
+    url = f'https://api.openweathermap.org/data/2.5/weather?\
+        q={city}&units=metric&lang=ru&appid={__api_key__}'
     weather_data = requests.get(url).json()
+    if weather_data['cod'] == 401:
+        print(weather_data)
+        return {
+            'response_code': 500,
+            'data': {
+                'message': 'Internal error'
+            }
+        }
+    elif weather_data['cod'] >= 400 and weather_data['cod'] < 500:
+        print(weather_data)
+        return {
+            'response_code': 400,
+            'data': {
+                'message': f'Incorrect city name: city={city}'
+            }
+        }
+    else:
+        return {
+        'response_code': 200,
+        'data': weather_data
+    }
 
-    temperature = round(weather_data['main']['temp'])
-    temperature_feels = round(weather_data['main']['feels_like'])
-
-    print(f'Current temperature in {city} is {temperature} degrees Celcius')
-    print(f'Feels like {temperature_feels}')
+def get_weather_hours(city: str, hours: int):
+    url = f'https://api.openweathermap.org/data/2.5/forecast?\
+        q={city}&units=metrics&lang=ru&appid={__api_key__}'
+    weather_data = requests.get(url).json()
+    if weather_data['cod'] == 401:
+        return {
+            'response_code': 500,
+            'data': {
+                'message': 'Internal error'
+            }
+        }
+    elif weather_data['cod'] >= 400 and weather_data['cod'] < 500:
+        return {
+            'response_code': 400,
+            'data': {
+                'message': f'Incorrect city name: city={city}'
+            }
+        }
+    else:
+        return {
+        'response_code': 200,
+        'data': weather_data['list'][hours : hours + 1]
+    }
